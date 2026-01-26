ba333c16643840a66ee1caeb96cb2b4e09f2cedf
RayminQAQ <raymin0425@gmail.com>
2026-01-26 19:25:12 +0800

    Level 2. Add, List

--- src/main/java/Rainbow.java
+++ src/main/java/Rainbow.java
@@ -1,6 +1,15 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Rainbow {
+
+    static ArrayList<String> storeItems = new ArrayList<>();
+
+    private static void printHorizontalLine() {
+        String horizontalLine = "____________________________________________________________";
+        System.out.println(horizontalLine);
+    }
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -9,30 +18,45 @@ public class Rainbow {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
 
-        String horizontalLine = "____________________________________________________________";
-
         // Rename & Greet
-        System.out.println(horizontalLine);
+        printHorizontalLine();
         System.out.println(" Hello! I'm Rainbow");
         System.out.println(" What can I do for you?");
-        System.out.println(horizontalLine);
 
-        // Level 1. Echo
+        // Level 1. Echo + Level 2. Add, List
         Scanner scanner = new Scanner(System.in);
-        while(true){
+        while (true) {
             String user_input = scanner.nextLine();
-            System.out.println(horizontalLine);
+            printHorizontalLine();
             System.out.println(" " + user_input);
-            System.out.println(horizontalLine);
 
-            if (user_input.equals("bye")){
+            if (user_input.equals("list")) {
+                // Print message
+                printHorizontalLine();
+                System.out.println(" Here are the items in your list:");
+                for (int i = 0; i < storeItems.size(); i++) { // Loop through internal storage
+                    System.out.println(" " + (i + 1) + ". " + storeItems.get(i));
+                }
+                printHorizontalLine();
+
+            } else if (user_input.equals("bye")) { // Exit
+                // Print message
+                printHorizontalLine();
+                System.out.println(" Bye. Hope to see you again soon!");
+                printHorizontalLine();
+
+                // Exit
                 break;
+            } else {
+                // Print message
+                printHorizontalLine();
+                System.out.println(" added: " + user_input);
+                printHorizontalLine();
+
+                // Internal storage
+                storeItems.add(user_input);
             }
         }
         scanner.close();
-
-        // Exit
-        System.out.println(" Bye. Hope to see you again soon!");
-        System.out.println(horizontalLine);
     }
 }
